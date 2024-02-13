165a727f27981bba916a65ea929fc77244aaa7a7
Xintong Zhang <zhang.xintong@northeastern.edu>
2024-02-13 07:57:08 -0800

    1

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 ## Personal Information:
 
-Full Name: Enter Your Full Name
+Full Name: XINTONG ZHANG
 
-Northeastern ID: Enter your Northeastern ID
 
 ## Getting Started
 First, fork this repository and clone it to your local machine.
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile.js'
+import React from 'react';
+import Profile from 'src/pages/qcomps/profile_mistake.js';
 
 export default function Home() {
   return (
